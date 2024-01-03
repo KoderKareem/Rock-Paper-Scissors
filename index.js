42ebd2341a39a55604abf1645d2c4704be192266
KoderKareem <abdulkarimsunusi2001@gmail.com>
2024-01-03 09:57:52 +0000

    I add a UI to the game

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let displayGameStatus = document.querySelector(".displayGameStatus");
 
 let displayStatus = document.createElement("p");
 displayStatus.style.fontSize = "20px";
+displayStatus.style.color = "rgb(240, 223, 238)";
+displayStatus.style.fontWeight = 900;
 // let displayPlayerStatus = document.createElement('p');
 
 function game() {
@@ -19,7 +21,7 @@ function game() {
   function updatePlayerScore(point) {
     PlayerScore += point;
   }
-  let PlayerScoreStatus = document.createElement("p");
+  let PlayerScoreStatus = document.querySelector(".playerStatus");
   function getPlayerScore() {
     return PlayerScore;
   }
@@ -29,7 +31,7 @@ function game() {
   function updateComputerScore(point) {
     computerScore += point;
   }
-  let ComputerScoreStatus = document.createElement("p");
+  let ComputerScoreStatus = document.querySelector(".computerStatus");
   function getComputerScore() {
     return computerScore;
   }
@@ -45,53 +47,41 @@ function game() {
       displayStatus.textContent = "You won, Rock beats Scissors.";
       displayGameStatus.appendChild(displayStatus);
       PlayerScoreStatus.textContent = `Your Score is: ${getPlayerScore()}`;
-      displayGameStatus.appendChild(PlayerScoreStatus);
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
       updateComputerScore(1);
 
       displayStatus.textContent = "You fail, Rock beats Scissors";
       displayGameStatus.appendChild(displayStatus);
       ComputerScoreStatus.textContent = `Computer Score is: ${getComputerScore()}`;
-      displayGameStatus.appendChild(ComputerScoreStatus);
     } else if (playerSelection == "scissors" && computerSelection == "papper") {
       updatePlayerScore(1);
       displayStatus.textContent = "You won, scissors beats papper.";
       displayGameStatus.appendChild(displayStatus);
       PlayerScoreStatus.textContent = `Your Score is: ${getPlayerScore()}`;
-      displayGameStatus.appendChild(PlayerScoreStatus);
     } else if (playerSelection == "papper" && computerSelection == "scissors") {
       updateComputerScore(1);
       displayStatus.textContent = "You fail, scissors beats papper";
       displayGameStatus.appendChild(displayStatus);
       ComputerScoreStatus.textContent = `Computer Score is: ${getComputerScore()}`;
-      displayGameStatus.appendChild(ComputerScoreStatus);
     } else if (playerSelection == "papper" && computerSelection == "rock") {
       updatePlayerScore(1);
       displayStatus.textContent = "You won, papper beats rock.";
       displayGameStatus.appendChild(displayStatus);
       PlayerScoreStatus.textContent = `Your Score is: ${getPlayerScore()}`;
-      displayGameStatus.appendChild(PlayerScoreStatus);
     } else if (playerSelection == "rock" && computerSelection == "papper") {
       updateComputerScore(1);
       displayStatus.textContent = "You fail, papper beats rock.";
       displayGameStatus.appendChild(displayStatus);
       ComputerScoreStatus.textContent = `Computer Score is: ${getComputerScore()}`;
-      displayGameStatus.appendChild(ComputerScoreStatus);
     } else if (computerSelection == "rock" && playerSelection == "rock") {
       if (playerSelection && computerSelection == "rock") {
         displayStatus.textContent = "A tie, Play Again. Rock & Rock";
         displayGameStatus.appendChild(displayStatus);
-        rockButton.addEventListener("click", handleButtonClick);
-        paperButton.addEventListener("click", handleButtonClick);
-        scissorsButton.addEventListener("click", handleButtonClick);
       }
     } else if (playerSelection == "papper" && computerSelection == "papper") {
       if (playerSelection && computerSelection == "papper") {
         displayStatus.textContent = "A tie, Play Again. papper & papper";
         displayGameStatus.appendChild(displayStatus);
-        rockButton.addEventListener("click", handleButtonClick);
-        paperButton.addEventListener("click", handleButtonClick);
-        scissorsButton.addEventListener("click", handleButtonClick);
       }
     } else if (
       playerSelection == "scissors" &&
@@ -100,9 +90,6 @@ function game() {
       if (playerSelection && computerSelection == "scissors") {
         displayStatus.textContent = "A tie, Play Again. scissors & scissors";
         displayGameStatus.appendChild(displayStatus);
-        rockButton.addEventListener("click", handleButtonClick);
-        paperButton.addEventListener("click", handleButtonClick);
-        scissorsButton.addEventListener("click", handleButtonClick);
       }
     }
   }
@@ -110,22 +97,20 @@ function game() {
   function handleButtonClick(event) {
     let playerSelection = event.target.textContent;
     let computerSelection = getComputerChoice();
-    let won = document.createElement("p");
-    won.style.fontSize = "30px";
+    let displayWins = document.querySelector(".displayWins");
+    displayWins.style.fontSize = "30px";
     // Call the playGame function with the playerSelection and computerSelection
     playGame(playerSelection, computerSelection);
 
     if (getPlayerScore() == 5 || getComputerScore() == 5) {
       if (getPlayerScore() == 5) {
-        won.textContent = `You won! Your final score is " ${getPlayerScore()}`;
-      displayGameStatus.appendChild(won);
-      } else if(getComputerScore() == 5){
-        won.textContent = `You fail! Computer final score is " ${getComputerScore()}`;
-        displayGameStatus.appendChild(won);
+        displayWins.textContent = `You won! Your final score is ${getPlayerScore()} and Computer's own is ${getComputerScore()}`;
+      } else if (getComputerScore() == 5) {
+        displayWins.textContent = `You fail! Computer's final score is ${getComputerScore()} and Yours is ${getPlayerScore()}`;
       }
       PlayerScore = 0;
       computerScore = 0;
-    } 
+    }
   }
 
   // add event listener to the button
